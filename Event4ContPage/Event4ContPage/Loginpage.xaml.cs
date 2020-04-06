046e2cacd6242a0cf4a856bc9742c50750ee7fba
sidhuraj <mr.prvsantosh@rediffmail.com>
2020-04-06 15:01:04 +0530

    first commit

---
+++ Event4ContPage/Event4ContPage/Loginpage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Event4ContPage
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Loginpage : ContentPage
+    {
+        public Loginpage()
+        {
+            InitializeComponent();
+
+            Title="welocme to login page";
+
+
+
+        }
+    }
+}
---
+++ Event4ContPage/Event4ContPage/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Event4ContPage
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnlogin.Clicked += Btnlogin_Clicked;
+            btnreset.Clicked += Btnreset_Clicked;
+
+            TapGestureRecognizer objUsr = new TapGestureRecognizer();
+            objUsr.Tapped += ObjUsr_Tapped;
+            lblNew.GestureRecognizers.Add(objUsr);
+            
+            
+        }
+
+        private void ObjUsr_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new UserPage());
+        }
+
+        private void Btnreset_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ResetPage());
+        }
+
+        private void Btnlogin_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Loginpage());
+        }
+    }
+}
